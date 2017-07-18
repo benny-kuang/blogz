8b0970d7a78b3ff1f20eefca2348fe8bb45320e1
Benny Kuang <bk.bennykuang@gmail.com>
2017-07-18 13:19:51 -0700

    Removed commented code that had been refactored to models and app files

--- main.py
+++ main.py
@@ -1,40 +1,5 @@
-# from flask import Flask, request, redirect, render_template, session, flash
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['DEBUG'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:benny@localhost:8889/blogz'
-# app.config['SQLALCHEMY_ECHO'] = True
-# db = SQLAlchemy(app)
-# app.secret_key = "secret"
-
 from app import *
 from models import *
-
-# # stores all the blog posts
-# class Blog(db.Model):
-
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(120))
-#     body = db.Column(db.String(500))
-#     owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
-#     def __init__(self, title, body, owner):
-#         self.title = title
-#         self.body = body
-#         self.owner = owner
-
-# # stores all users
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(120), unique=True)
-#     password = db.Column(db.String(120))
-#     posts = db.relationship('Blog', backref='owner')
-
-#     def __init__(self, username, password):
-#         self.username = username
-#         self.password = password
     
 @app.route('/', methods=['GET'])
 def index():
